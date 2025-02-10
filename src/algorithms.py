b965f858e2486ead5f99fa05e797c21d1606485d
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2025-02-10 21:53:55 +0100

    optimize seidel algorithm

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,22 +1,24 @@
 from typing import Callable
 import random
 
-def random_permutation(set_:set[int]) -> list[int]:
-    permutation = list(set_)
-    size = len(permutation)
+def random_permutation(list_:list[int]) -> list[int]:
+    size = len(list_)
     for i in range(size - 1):
         j = random.randint(i, size - 1)
-        permutation[i], permutation[j] = permutation[j], permutation[i]
-    return permutation
+        list_[i], list_[j] = list_[j], list_[i]
+    return list_
 
 
-def seidel(global_set:set[int], func:Callable, possible_set:set[int]) -> set[int]:
-    rejected_set = set()
+def seidel(global_set:list[int], func:Callable, possible_set:list[int]) -> list[int]:
+    rejected_set = []
     critical_set = possible_set
+    
+    ball_from_critical_set = None
 
     for i in random_permutation(global_set):
-        if func(critical_set) != func(critical_set | {i}):
-            critical_set = seidel(rejected_set, func, possible_set | {i})
-        rejected_set = rejected_set | {i}
+        ball_from_critical_set, critical_set_has_changed = func(critical_set, i)
+        if critical_set_has_changed:
+            ball_from_critical_set, critical_set = seidel(rejected_set, func, possible_set + [i])
+        rejected_set.append(i)
     
-    return critical_set
+    return ball_from_critical_set, critical_set
